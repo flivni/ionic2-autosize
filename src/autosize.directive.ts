d367446de978b9a26f905906309b4dc593075701
Felix Livni <flivni@gmail.com>
2016-08-17 18:05:06 -0700

    ws

--- src/autosize.directive.ts
+++ src/autosize.directive.ts
@@ -1,20 +1,23 @@
-import { ElementRef, HostListener, Directive} from '@angular/core';
+import {ElementRef, HostListener, Directive} from '@angular/core';
 
 @Directive({
-    selector: 'ion-textarea[autosize]'
+  selector: 'ion-textarea[autosize]'
 })
 
 export class Autosize {
- @HostListener('input',['$event.target'])
-  onInput(textArea: HTMLTextAreaElement): void {
+  @HostListener('input', ['$event.target'])
+  onInput(textArea:HTMLTextAreaElement):void {
     this.adjust();
   }
-  constructor(public element: ElementRef){
+
+  constructor(public element:ElementRef) {
   }
-  ngAfterViewInit(): void{
+
+  ngAfterViewInit():void {
     this.adjust();
   }
-  adjust(): void{
+
+  adjust():void {
     let textArea = this.element.nativeElement.getElementsByTagName('textarea')[0];
     textArea.style.overflow = 'hidden';
     textArea.style.height = 'auto';
